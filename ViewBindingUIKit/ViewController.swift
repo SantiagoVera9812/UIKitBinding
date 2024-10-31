7c23c1873ae1ed03507c0df73fc03352dd0d988d
Christian Santiago Vera Rojas <christian.vera@globant.com>
2024-10-31 16:26:46 -0500

    Initial Commit

---
+++ ViewBindingUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewBindingUIKit
+//
+//  Created by Christian Santiago Vera Rojas on 31/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ViewBindingUIKitTests/ViewBindingUIKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  ViewBindingUIKitTests.swift
+//  ViewBindingUIKitTests
+//
+//  Created by Christian Santiago Vera Rojas on 31/10/24.
+//
+
+import Testing
+@testable import ViewBindingUIKit
+
+struct ViewBindingUIKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
